1f804baa3390cb7220a5968764b0d86c0a026185
billgrah48 <billy_graham@hotmail.co.uk>
2026-02-19 16:15:09 +0000

    test

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Hello World</title>
-        <style>
-            body {
-                background-color: blue;
-                color: white;
-                font-family: Arial, sans-serif;
-                text-align: center;
-                padding-top: 20%;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Hello, World!</h1>
-    </body>
-    </html>
-    """
+@app.route('/')
+def hello_world():
+    return 'Well done Bill Be more creative a!'
